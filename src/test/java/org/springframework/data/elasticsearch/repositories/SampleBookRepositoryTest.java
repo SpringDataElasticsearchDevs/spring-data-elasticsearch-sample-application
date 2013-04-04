dcd735d4e03e06fd8230c5f4f5230315796329d2
Mohsin Husen <smhdiu@gmail.com>
2013-04-04 17:55:04 +0100

    Test added for custom method using repository

--- src/test/java/org/springframework/data/elasticsearch/repositories/SampleBookRepositoryTest.java
+++ src/test/java/org/springframework/data/elasticsearch/repositories/SampleBookRepositoryTest.java
@@ -172,8 +172,6 @@ public class SampleBookRepositoryTest {
     public void shouldReturnBooksWithName(){
         Book book1 = new Book(RandomStringUtils.random(5),"test1",System.currentTimeMillis());
         Book book2 = new Book(RandomStringUtils.random(5),"test2",System.currentTimeMillis());
-        book1.setPrice(10L);
-        book2.setPrice(10L);
         repository.save(Arrays.asList(book1,book2));
 
         Page<Book> books = repository.findByName("test1", new PageRequest(0,10));
